0c05e24467998976c38f13aae4df414f32cdeb89
Béla Kopasz <kopaszbeci@gmail.com>
2026-02-03 10:09:09 +0100

    email validálás félbehagyva

--- WPFregiszracio/MainWindow.xaml.cs
+++ WPFregiszracio/MainWindow.xaml.cs
@@ -27,25 +27,47 @@ namespace WPFregiszracio
             InitializeComponent();
         }
 
+        private bool IsValidEmail(string email) 
+        {
+            int  atIndex = email.IndexOf("@");
+            if (atIndex < 0 || atIndex!=email.LastIndexOf("@")) return false;
+            string local = email.Substring(0,atIndex);
+            string domain = email.Substring(atIndex+1);
+
+            if (string.IsNullOrEmpty(local)||string.IsNullOrEmpty(domain)) return false;
+
+
+            return true;
+        }
+
         private void btn_regisztracio_Click(object sender, RoutedEventArgs e)
         {
-            if (feltetel.IsChecked==true && tbx_jelszo1.Text==tbx_jelszo2.Text)
+            if (string.IsNullOrWhiteSpace(tbx_nev.Text)||string.IsNullOrWhiteSpace(tbx_email.Text)|| string.IsNullOrWhiteSpace(tbx_jelszo1.Text) || string.IsNullOrWhiteSpace(tbx_jelszo2.Text))
             {
-                string nev = tbx_nev.Text;
-                string email = tbx_email.Text;
-                string jelszo = tbx_jelszo1.Text;
-                var egyFelhasznalo = new User(nev, email, jelszo);
-                felhasznalok.Add(egyFelhasznalo);
-                using (StreamWriter iro = new StreamWriter("felhasznalok.txt", true))
-                {
-                    iro.WriteLine($"{egyFelhasznalo.Nev};{egyFelhasznalo.Email};{egyFelhasznalo.Jelszo}");
-                }
-                MessageBox.Show("Sikeres regisztráció!");
+                MessageBox.Show("A mezők kitöltése kötelező!", "Hiba!", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
             else
             {
-                MessageBox.Show("Fogadd el feltételeket, vagy ellenőrizd a jelszavak egyezőségét!", "Hiba!", MessageBoxButton.OK,MessageBoxImage.Warning);
+                if (feltetel.IsChecked == true && tbx_jelszo1.Text == tbx_jelszo2.Text)
+                {
+                    string nev = tbx_nev.Text;
+                    string email = tbx_email.Text;
+                    string jelszo = tbx_jelszo1.Text;
+                    var egyFelhasznalo = new User(nev, email, jelszo);
+                    felhasznalok.Add(egyFelhasznalo);
+                    using (StreamWriter iro = new StreamWriter("felhasznalok.txt", true))
+                    {
+                        iro.WriteLine($"{egyFelhasznalo.Nev};{egyFelhasznalo.Email};{egyFelhasznalo.Jelszo}");
+                    }
+                    MessageBox.Show("Sikeres regisztráció!");
+                }
+                else
+                {
+                    MessageBox.Show("Fogadd el feltételeket, vagy ellenőrizd a jelszavak egyezőségét!", "Hiba!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
             }
+            
+            
             
         }
     }
